6eb89c39f682dca7a39f017ccc9b53a53e4910f7
Max Meyerson <maxgmeyerson@gmail.com>
2018-11-01 16:29:31 -0400

    Add files via upload

---
+++ On_Off_Switch_350_SensorLab.ino
@@ -0,0 +1,26 @@
+int onoffSwitch = 0;
+
+const int onoffSwitchPin = 5;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Start");
+  pinMode(onoffSwitchPin, INPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  onoffSwitch = digitalRead(onoffSwitchPin);
+
+  if (onoffSwitch == HIGH) {
+
+    Serial.println("Switch on");
+  } 
+  else{
+    Serial.println("Switch off");
+  }
+  
+  
+}
